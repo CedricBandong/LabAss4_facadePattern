896f52f6d28f1f717ff16bedf9810145c0b39e70
Cedric Bandong <83577761+CedricBandong@users.noreply.github.com>
2024-10-24 01:57:00 +0800

    Create Cart.java

---
+++ Cart.java
@@ -0,0 +1,9 @@
+public class Cart implements HotelService{
+    public void requestCart(int numberOfCarts) {
+      if (numberOfCarts == 1) {
+        System.out.println(numberOfCarts + " cart has been requested.");
+      }
+      else {
+        System.out.println(numberOfCarts + " carts has been requested.");
+    }
+}
